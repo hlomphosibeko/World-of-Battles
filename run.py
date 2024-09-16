81e36312851e825a4461dd6b3025cdfef02ab810
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-09-16 21:46:35 +0000

    Create functions for the game, remove redundant code

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+rows = 5
+columns = 6
+
+class Game:
+    """
+    This is the battleship game. 
+    A player will be playing against the computer
+    """
+    def get_player_name():
+        pass
+
+
+    def random_pick():
+        pass   
+
+    def play_again():
+        pass
+    
+
+
+    play_again()
+
