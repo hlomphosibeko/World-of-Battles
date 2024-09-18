9e82e674a0720329152f1d6261920e629b7302a8
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-09-18 07:23:12 +0000

    Add game reload function which sets the board size, delete redundant code

--- run.py
+++ run.py
@@ -11,16 +11,25 @@ class Board:
     """
     def __init__(self, size, board, num_ships, name, type):
         self.size = size
-        self.board = [["." for x in range(size)] for y in range(size)]
+        #self.board = [["." for x in range(size)] for y in range(size)]
         self.num_ships = num_ships
         self.type = type
         self.guesses = []
         self.ships = []
 
 
-
-    def random_pick():
-        pass   
+    def game_reload():
+        """
+        This function reloads a new game. It resets the board size and 
+        the position of the ships
+        """  
+        size = 7
+        num_ships = 5
+        scores["computer"] = 0
+        scores["player"] = 0
+        #print("-" * 35)
+        print("Welcome to WORLD OF BATTLES!!")
+       
 
     def play_again():
         pass
