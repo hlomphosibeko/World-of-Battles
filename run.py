8b0f5d1e1dac9f6f4e2055bc43e3d60bd8e6f639
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-09-19 06:53:32 +0000

    Add docstring to the board class, adjust __init__ method, delete redundant code

--- run.py
+++ run.py
@@ -1,19 +1,18 @@
 from random import randint
 
 scores = {"player1": 0, "computer": 0}
-rows = 5
-columns = 6
 
 class Board:
     """
-    This is the main board. This class sets the number of ships, 
-    it allows the player to play against the computer. 
+    This is the main board. This class sets the board size, number of ships, 
+    and player1's name. It allows the player to play against the computer. 
     
     """
-    def __init__(self, size, board, num_ships, name, type):
+    def __init__(self, size, num_ships, name, type):
         self.size = size
-        #self.board = [["." for x in range(size)] for y in range(size)]
+        self.board = [["." for x in range(size)] for y in range(size)]
         self.num_ships = num_ships
+        self.name = name
         self.type = type
         self.guesses = []
         self.ships = []
